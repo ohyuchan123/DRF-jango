175f073d4daf5403c82d43dd75d1f3a004c57353
오유찬 <oyuchan50@gmail.com>
2023-04-18 20:26:34 +0900

    :sparkles: Serializer 생성 - Django Rest Framework Serializer에 대한 기능들을 공부하였습니다.

--- sinppets/models.py
+++ sinppets/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 
 # Create your models here.
+class Person(models.Model):
+    id = models.IntegerField()
+    name = models.CharField()
+    phone = models.CharField()
+    email = models.CharField()
