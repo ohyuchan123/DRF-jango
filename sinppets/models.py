97e68b90a8b6e5a82e0010d82f589d3dce398114
오유찬 <oyuchan50@gmail.com>
2023-04-21 12:16:46 +0900

    :ambulance: 오타

--- sinppets/models.py
+++ sinppets/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-    id = models.IntegerField()
-    name = models.CharField()
-    phone = models.CharField()
-    email = models.CharField()
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
