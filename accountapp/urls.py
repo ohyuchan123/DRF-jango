cf0b1b3ef669784fe9e1ed0ad409351e054f7f74
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2023-05-16 15:25:04 +0900

    :fire: 파일 삭제

--- accountapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from .views import hello_world,hello_world_drf
-
-# url은 언더바(_)보단 하이픈(-)으로 작성하자
-urlpatterns = [
-    path('hello-world/',hello_world),
-    path('hello-world-drf/',hello_world_drf),
-]
